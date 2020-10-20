48a591f4e2e52086d2c34c83ce08d044f8aafaae
adilasomaaaa <yasdilofficial@gmail.com>
2020-10-20 14:30:44 +0800

    style

--- module_cms/wordpress/wp-content/themes/blankslate/entry-summary.php
+++ module_cms/wordpress/wp-content/themes/blankslate/entry-summary.php
@@ -1,9 +1,6 @@
 <div class="entry-summary">
 <?php if ( has_post_thumbnail() ) : ?>
-<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-<!-- <div class="small_thumb"> -->
-    <?php the_post_thumbnail('small_thumb'); ?>
-</a>
+
 <!-- </div> -->
 <?php endif; ?>
 <?php echo get_the_excerpt(); ?>
--- module_cms/wordpress/wp-content/themes/blankslate/entry.php
+++ module_cms/wordpress/wp-content/themes/blankslate/entry.php
@@ -5,12 +5,22 @@ echo '<h1 class="entry-title">';
 } else {
 echo '<h2 class="entry-title">';
 } ?>
+
+<?php if ( is_singular() ){
+    echo  '<img class="attachment-small_thumb size-small_thumb wp-post-image" />';
+}
+?>
+<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+<?php the_post_thumbnail('small_thumb'); ?>
+</a>
+
+
 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
 <?php if ( is_singular() ) {
 echo '</h1>';
 } else {
 echo '</h2>';
-} ?> <?php edit_post_link(); ?>
+} ?> 
 <?php if ( ! is_search() ) { get_template_part( 'entry', 'meta' ); } ?>
 </header>
 <?php get_template_part( 'entry', ( is_front_page() || is_home() || is_front_page() && is_home() || is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
